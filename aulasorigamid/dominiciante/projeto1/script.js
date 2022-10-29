8106186b160d40648f51ebacbaa47b808d3203c1
liviafort <livia.fortunato@academico.ifpb.edu.br>
2022-10-29 11:20:29 -0300

    Seleção de elementos parte 2

--- aulasorigamid/dominiciante/projeto1/script.js
+++ aulasorigamid/dominiciante/projeto1/script.js
@@ -1,4 +1,5 @@
 //seleção de elementos
+
 const computadoresSection = document.getElementById('computadores');
 console.log(computadoresSection);
 console.log(computadoresSection.innerText);
@@ -26,3 +27,35 @@ const listas = document.querySelectorAll('ul');
 const titulos = document.querySelectorAll('.titulo');
 const fotosComputadores = document.querySelectorAll('.computadores-lista img');
 console.log(fotosComputadores);
+//HTMLCOLLECTION VS NODELIST
+const titulo = document.querySelector('.titulo');
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+titulo.classList.add('grid-section');
+console.log(gridSectionHTML); // 4 itens
+console.log(gridSectionNode); // 3 itens
+//ARRAY-LIKE
+gridSection.forEach(function(item, array){
+  console.log(item, array);
+});
+const arrayGrid = Array.from(gridSectionHTML);
+console.log(arrayGrid);
+//exercicio
+//retornar todas as imagens:
+const todasimg = document.querySelectorAll
+('img');
+console.log(todasimg);
+//retornar imagens que começam com a palavra imagem
+const imagenspc = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imagenspc);
+//selecionar links internos onde href começa com #
+const links = document.querySelectorAll('[href^="https://"]');
+console.log(links);
+//selecione primeiro h2 dentro de animais-descrição
+const animaisdescricao = document.getElementsByTagName('h2');
+console.log(animaisdescricao[1]);
+//ultimo p do site
+const ultimop = document.getElementsByTagName('p');
+const tamanhop = ultimop.length;
+console.log(ultimop[tamanhop - 1]);
+
