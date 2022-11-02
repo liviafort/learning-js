764cda19c51d6dbcad367737dac7057e7a20abb7
liviafort <livia.fortunato@academico.ifpb.edu.br>
2022-11-01 22:03:15 -0300

    Novo método de estudo

---
+++ aulasjsauto/aula1.js
@@ -0,0 +1,11 @@
+const nome = "LIVIAAAAAAA";
+//alert(nome);
+console.log(`meu nome é ${nome} e meu nome possui ${nome.length} letras`);
+const nome2 = "felipe,oliveira";
+console.log(nome2.split(','));
+//booleanos
+console.log(2==3);
+console.log(2==2);
+//null & undefined
+const x = null;
+const y = undefined;
